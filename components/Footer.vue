698383ab6b2ec28f706393d018b1d39093772d87
kakelay <kakelay18052002@gmail.com>
2023-09-29 02:15:43 +0700

    first commit

---
+++ components/Footer.vue
@@ -0,0 +1,29 @@
+<!-- components/Header.vue -->
+<template>
+  <footer class="bg-gray-900 p-20">
+    <div class="text-center">
+      <h1 class="text-white text-1xl">
+        Women’s leadership and political participation Economic empowerment
+        Ending violence against women Peace and security Humanitarian action
+        Governance and national planning Youth Women and girls with disabilities
+        Sustainable development agenda HIV and AIDS
+      </h1>
+    </div>
+  </footer>
+</template>
+
+
+<script>
+export default {
+  //   data() {
+  //     return {
+  //       isDarkMode: false,
+  //     };
+  //   },
+  //   methods: {
+  //     toggleDarkMode() {
+  //       this.isDarkMode = !this.isDarkMode;
+  //     },
+  //   },
+};
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        'sm': '640px', // Small screens
+        'md': '768px', // Medium screens
+        'lg': '1024px', // Large screens
+        'xl': '1280px', // Extra-large screens
+      },
+    },
+  },
+  plugins: [],
+  darkMode: true,
+}
